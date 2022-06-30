b18fe3dac79f3115cd132d7de79577764a490e18
scottney <mamaiscotty@gmail.com>
2022-06-30 12:01:04 -0700

    Commit 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,20 @@
+#!/bin/bash
+include <stdio.h>
+
+/**
+ *
+ *  * main - main block
+ *
+ *   * Return: 0
+ *
+ *    */
+
+int main(void)
+
+{
+
+		printf("with proper grammar, but the outcome is a piece of art,\n");
+
+			return (0);
+
+}
